590d6540355f57e25452432264f7badc6b46b4b0
Julien Lebron <lebron-pro@outlook.fr>
2025-04-26 22:37:11 +0200

    feat: listing screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,12 @@
 import { Link } from "expo-router";
-import { StatusBar, StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ThemedText } from "./components/ThemedText";
 import { useThemeColors } from "@/hooks/useThemeColors";
@@ -7,16 +14,49 @@ import { Card } from "./components/Card";
 
 export default function Index() {
   const colors = useThemeColors();
+  const pokemons = Array.from({ length: 35 }, (_, k) => ({
+    name: "Pokémon name",
+    id: k + 1,
+  }));
   return (
     <SafeAreaView style={[styles.container, { backgroundColor: colors.tint }]}>
       <StatusBar backgroundColor={colors.tint} barStyle="light-content" />
-      <Card>
-        <ThemedText variant="headline">Pokédex</ThemedText>
+      <View style={styles.header}>
+        <Image
+          source={require("@/assets/images/pokeball.png")}
+          width={24}
+          height={24}
+        />
+        <ThemedText variant="headline" color="grayLight">
+          Pokédex
+        </ThemedText>
+      </View>
+      <Card style={styles.body}>
+        <FlatList
+          data={pokemons}
+          numColumns={3}
+          contentContainerStyle={[styles.gridGap, styles.list]}
+          columnWrapperStyle={styles.gridGap}
+          renderItem={({ item }) => (
+            <Card style={{ flex: 1 / 3, height: 200 }}>
+              <Text>{item.name}</Text>
+            </Card>
+          )}
+          keyExtractor={(item) => item.id.toString()}
+        />
       </Card>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, paddingTop: 24 },
+  container: { flex: 1, padding: 4 },
+  header: { flexDirection: "row", alignItems: "center", gap: 16, padding: 12 },
+  body: { flex: 1 },
+  gridGap: {
+    gap: 8,
+  },
+  list: {
+    padding: 12,
+  },
 });
